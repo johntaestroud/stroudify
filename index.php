44f4c5ff7aa91af57d25d093819afaec7a1ace3a
Johntae Stroud <johntae.stroud@gmail.com>
2018-03-18 13:48:01 -0400

    added header and footer files

--- index.php
+++ index.php
@@ -1,41 +1,10 @@
-<?php
-include("includes/config.php");
-
-//session_destroy();
-
-if(isset($_SESSION['userLoggedIn'])) {
-	$userLoggedIn = $_SESSION['userLoggedIn'];
-} else {
-	#header("Location: register.php");
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Welcome to Stroudify</title>
-
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-<body>
-
-	<div class="mainContainer">
-
-		<div class="topContainer">
-			
-			<?php include("includes/navBarContainer.php"); ?>
-
-		</div>
- 	 
-			<?php include("includes/nowPlayingBar.php"); ?>
-
-			
-
-	</div>
+<?php include("includes/header.php");?>
+<?php include("includes/footer.php");?>
 
 
-		
 
 
-</body>
-</html>
+
+
+
+					
