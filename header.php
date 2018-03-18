654d349693f3ace0c1a1fdd91eb3b2c5938dc199
Johntae Stroud <johntae.stroud@gmail.com>
2018-03-18 15:15:55 -0400

    styling the album

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+include("includes/config.php");
+
+//session_destroy();
+
+if(isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+	#header("Location: register.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Stroudify</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+	<div class="mainContainer">
+
+		<div class="topContainer">
+			
+			<?php include("includes/navBarContainer.php"); ?>
+
+			<div id="mainViewContainer">
+				
+				<div id="mainContent">
