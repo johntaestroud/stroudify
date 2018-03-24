98d226f08bdee138b7e7678c53401f67f1262fe1
Johntae Stroud <johntae.stroud@gmail.com>
2018-03-24 13:54:42 -0400

    formatted time remaining

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 var currentPlaylist = [];
 var audioElement;
 
+function formatTime(seconds) {
+	var time = Math.round(seconds);
+	var minutes = Math.floor(time / 60); 
+	var seconds = time - (minutes * 60);
 
+	var extraZero = (seconds < 10) ? "0" : "";
+	
+	return minutes + ":" + extraZero + seconds;
+}
 
 function Audio() {
 	
@@ -11,7 +19,8 @@ function Audio() {
 
 	this.audio.addEventListener("canplay", function() {
 		//referring to the obj the event was called on
-		$(".progressTime.remaining").text(this.duration);
+		var duration = formatTime(this.duration)
+		$(".progressTime.remaining").text(duration);
 	});
 
 	this.setTrack = function(track) {
