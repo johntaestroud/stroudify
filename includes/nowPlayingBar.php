06f2c8651d74b0c2ed7e10b9a9201914bf71ab18
Johntae Stroud <johntae.stroud@gmail.com>
2018-03-24 16:09:53 -0400

    added mute functionality

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -114,6 +114,12 @@ $jsonArray = json_encode($resultArray);
 		// }
 	}
 
+	function setMute() {
+		audioElement.audio.muted = !audioElement.audio.muted;
+		var imageName = audioElement.audio.muted ? "volume-mute.png" : "volume.png";
+		$(".controlButton.volume img").attr("src", "assets/images/icons/" + imageName);
+	}
+
 	function setTrack(trackId, newPlaylist, play) {
 
 		currentIndex = currentPlaylist.indexOf(trackId);
@@ -245,7 +251,7 @@ $jsonArray = json_encode($resultArray);
 		<div id="nowPlayingRight">
 			<div class="volumeBar">
 
-				<button class="controlButton volume" title="Volume button">
+				<button class="controlButton volume" title="Volume button" onclick="setMute()">
 					<img src="assets/images/icons/volume.png" alt="Volume">
 				</button>
 
