72ffaab217f60c9a5fe74dca670c1e0eab897590
Johntae Stroud <johntae.stroud@gmail.com>
2018-03-24 13:32:24 -0400

    displaying timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,11 @@ function Audio() {
 	//creating HTML element
 	this.audio = document.createElement('audio');	
 
+	this.audio.addEventListener("canplay", function() {
+		//referring to the obj the event was called on
+		$(".progressTime.remaining").text(this.duration);
+	});
+
 	this.setTrack = function(track) {
 		this.currentlyPlaying = track; //keeping track of the song currently playing everytime we set the track
 		this.audio.src = track.path; //path is the name in the db 
